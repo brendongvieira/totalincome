19fee7a6080096287e4d28d4da080dbb31d82f93
Brendon Vieira <brendon-vieira@live.com>
2024-01-06 20:19:09 -0300

    First commit

---
+++ Income/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿namespace Income.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
